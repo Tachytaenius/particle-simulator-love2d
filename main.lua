849b1390b6582649240b7baa861034e3a261144a
Tachytaenius <henryfthomson@gmail.com>
2023-08-06 22:28:30 +0100

    Experiment with chance-based emission & absorption

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
 math.tau = math.pi * 2
+function math.lerp(a, b, i)
+	return a + i * (b - a)
+end
 
 local vec2 = require("lib.mathsies").vec2
 local list = require("lib.list")
@@ -7,7 +10,7 @@ local particles
 local spawnIfHoldingShift
 
 -- Functions
-local newParticle
+local newParticle, absorbParticle
 
 local function randCircle(r)
 	return vec2.rotate(vec2(love.math.random() ^ 0.5 * r, 0), love.math.random() * math.tau)
@@ -17,24 +20,51 @@ end
 	Charges:
 	Electric: Real number
 	Personal space: Positive real number
+	Strange A: Real number
+	Strange B: Real number
 ]]
 
 function love.load()
 	particles = list()
 
-	for _=1, 500 do
-		local massive = love.math.random() > 0.25
-		local pos = randCircle(100)
+	for _=1, 100 do
+		local pos = randCircle(300)
 		newParticle(
 			{
-				electric = massive and 0 or love.math.random() > 0.5 and 1 or -1,
-				personalSpace = massive and 1 or 0
+				electric = 0,
+				personalSpace = 1,
+				strangeA = 1,
+				strangeB = 0
 			},
-			massive and 1000 or 1,
+			1,
 			pos + vec2(500, 400),
 			vec2(-pos.y, pos.x) * 0.05
 		)
 	end
+
+	newParticle(
+		{
+			electric = 0,
+			personalSpace = 1,
+			strangeA = 1,
+			strangeB = 0
+		},
+		1,
+		vec2(300, 300),
+		vec2()
+	)
+
+	newParticle(
+		{
+			electric = 0,
+			personalSpace = 1,
+			strangeA = 1,
+			strangeB = 0
+		},
+		1,
+		vec2(500, 300),
+		vec2()
+	)
 end
 
 function love.mousepressed()
@@ -49,7 +79,9 @@ function love.update(dt)
 			newParticle(
 				{
 					electric = 1,
-					personalSpace = 0
+					personalSpace = 0,
+					strangeA = 0,
+					strangeB = 0
 				},
 			1, vec2(love.mouse.getPosition()) + randCircle(brushRadius))
 		end
@@ -57,14 +89,18 @@ function love.update(dt)
 			newParticle(
 				{
 					electric = -1,
-					personalSpace = 0
+					personalSpace = 0,
+					strangeA = 0,
+					strangeB = 0
 				},
 			1, vec2(love.mouse.getPosition()) + randCircle(brushRadius))
 		end
 		if love.mouse.isDown(3) then
 				newParticle({
 					electric = 0,
-					personalSpace = 1
+					personalSpace = 1,
+					strangeA = 0,
+					strangeB = 0
 				},
 			1000, vec2(love.mouse.getPosition()) + randCircle(brushRadius))
 		end
@@ -75,9 +111,26 @@ function love.update(dt)
 	local timestepDivisions = 1
 	for i = 1, timestepDivisions do
 		local dt = dt / timestepDivisions
+
 		for i = 1, particles.size do
 			local particle = particles:get(i)
 			particle.position = particle.position + particle.velocity * dt
+		end
+
+		local particlesToSpawn = {}
+		local particlesToDelete = {}
+		function absorbParticle(absorber, absorbee)
+			absorbee.ignore = true
+			particlesToDelete[#particlesToDelete + 1] = absorbee
+
+			absorber.charges.electric = absorber.charges.electric + absorbee.charges.electric
+			absorber.charges.strangeA = absorber.charges.strangeA + absorbee.charges.strangeB
+			absorber.charges.strangeB = absorber.charges.strangeB + absorbee.charges.strangeA -- What are the implications of this?
+
+			local i = absorbee.mass / (absorber.mass + absorbee.mass)
+			absorber.position = math.lerp(absorber.position, absorbee.position, i)
+			absorber.velocity = absorber.velocity + absorbee.velocity
+			absorber.mass = absorber.mass + absorbee.mass
 		end
 		for i = 1, particles.size - 1 do
 			local particleA = particles:get(i)
@@ -85,39 +138,117 @@ function love.update(dt)
 			for j = i + 1, particles.size do
 				local particleB = particles:get(j)
 
-				local difference = particleB.position - particleA.position
-				local distance = #difference
-				if distance > 0 then
-					local direction = difference / distance
-					local force = 0
+				if not particleA.ignore and not particleB.ignore then
+					local difference = particleB.position - particleA.position
+					local distance = #difference
+					if distance > 0 then
+						local direction = difference / distance
+						local force = 0
 
-					-- Electric force
-					local electricForceStrength = 500
-					force = force + electricForceStrength * -1 * particleA.charges.electric * particleB.charges.electric * math.min(1, distance ^ -1)
+						-- Electric force
+						local electricForceStrength = 500
+						force = force + electricForceStrength * -1 * particleA.charges.electric * particleB.charges.electric * math.min(1, distance ^ -1)
 
-					-- Gravity
-					local gravitationalForceStrength = 1
-					force = force + gravitationalForceStrength * particleA.mass * particleB.mass * math.min(1.0, distance ^ -1)
+						-- Gravity
+						local gravitationalForceStrength = 1
+						force = force + gravitationalForceStrength * particleA.mass * particleB.mass * math.min(1.0, distance ^ -1)
 
-					-- Personal space force
-					local personalSpaceForceStrength = 2000
-					force = force + -1 * personalSpaceForceStrength * particleA.charges.personalSpace * particleB.charges.personalSpace * math.min(1.0, distance ^ -4)
-						* particleA.mass * particleB.mass * math.min(1, math.max(0, -vec2.dot(difference, particleB.velocity - particleA.velocity)))
+						-- Personal space force
+						local personalSpaceForceStrength = 2000
+						force = force + -1 * personalSpaceForceStrength * particleA.charges.personalSpace * particleB.charges.personalSpace * math.min(1.0, distance ^ -4)
+							* particleA.mass * particleB.mass * math.min(1, math.max(0, -vec2.dot(difference, particleB.velocity - particleA.velocity)))
 
-					force = force * direction
-					particleA.velocity = particleA.velocity + force / particleA.mass * dt
-					particleB.velocity = particleB.velocity - force / particleB.mass * dt
+						-- Strange "force" emission
+						local strangeForceBaseEmissionChancePerSecond = 0.1
+						local strangeForceEmittedSlowness = 10
+						if particleA.charges.strangeA ~= 0 and particleB.charges.strangeA ~= 0 then -- TODO: Replace with multiplication?
+							local strangeForceBaseEmissionChanceThisTick = 1 - (1 - strangeForceBaseEmissionChancePerSecond) ^ dt
+							local function tryEmitAToB()
+								if particleA.charges.strangeA ~= 0 and love.math.random() < strangeForceBaseEmissionChanceThisTick then
+									particlesToSpawn[#particlesToSpawn + 1] = {
+										{
+											electric = 0,
+											personalSpace = 0,
+											strangeA = 0,
+											strangeB = particleA.charges.strangeA
+										},
+										0,
+										particleA.position,
+										particleA.velocity + direction * distance / strangeForceEmittedSlowness
+									}
+									particleA.charges.strangeA = 0
+									particleA.velocity = particleA.velocity - direction * distance / strangeForceEmittedSlowness
+								end
+							end
+							local function tryEmitBToA()
+								if particleB.charges.strangeA ~= 0 and love.math.random() < strangeForceBaseEmissionChanceThisTick then
+									particlesToSpawn[#particlesToSpawn + 1] = {
+										{
+											electric = 0,
+											personalSpace = 0,
+											strangeA = 0,
+											strangeB = particleB.charges.strangeA
+										},
+										0,
+										particleB.position,
+										particleB.velocity - direction * distance / strangeForceEmittedSlowness
+									}
+									particleB.charges.strangeA = 0
+									particleB.velocity = particleB.velocity + direction * distance / strangeForceEmittedSlowness
+								end
+							end
+							if love.math.random() < 0.5 then
+								tryEmitAToB()
+								tryEmitBToA()
+							else
+								tryEmitBToA()
+								tryEmitAToB()
+							end
+						end
+
+						force = force * direction
+						if particleA.mass ~= 0 then
+							particleA.velocity = particleA.velocity + force / particleA.mass * dt
+						end
+						if particleB.mass ~= 0 then
+							particleB.velocity = particleB.velocity - force / particleB.mass * dt
+						end
+					end
+					-- Strange "force" absorbtion
+					local strangeForceBaseAbsorptionChancePerSecond = 0.2
+					local function tryAAbsorbB()
+						if particleB.charges.strangeB ~= 0 then
+							local distFactor
+							if distance > 0 then
+								distFactor = math.min(1, distance ^ -0.0001)
+							else
+								distFactor = 1
+							end
+							local absorptionChancePerSecond = strangeForceBaseAbsorptionChancePerSecond * distFactor
+							local absorptionChanceThisTick = 1 - (1 - absorptionChancePerSecond) ^ dt
+							if love.math.random() < absorptionChanceThisTick then
+								absorbParticle(particleA, particleB)
+							end
+						end
+					end
+					tryAAbsorbB()
 				end
 			end
+		end
+		for _, particle in ipairs(particlesToDelete) do
+			particles:remove(particle)
+		end
+		for _, newParticleParameters in ipairs(particlesToSpawn) do
+			newParticle(unpack(newParticleParameters))
 		end
 	end
 end
 
 function love.draw()
-	love.graphics.setPointSize(2)
+	love.graphics.setPointSize(3)
 	for i = 1, particles.size do
 		local particle = particles:get(i)
-		love.graphics.setColor(particle.charges.electric, 0.5, -particle.charges.electric)
+		love.graphics.setColor(particle.charges.strangeA / 2 + 0.5, particle.charges.strangeB / 2 + 0.5, 0.5)
 		love.graphics.points(particle.position.x, particle.position.y)
 	end
 end
